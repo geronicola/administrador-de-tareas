2e06bb39fb50377506dd326e603c4439ecc17d10
gero <geronicola1696@gmail.com>
2022-10-06 13:30:17 -0300

    Proyecto Terminado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Botones
 const btnAdd = document.getElementById("add-category");
 const deleteCategory = document.getElementById("delete-category")
+const btnEdit = document.getElementById("editar-category")
 const showCategory = document.getElementById("show-category")
 
 const inputCategory = document.getElementById("category")
@@ -10,68 +11,94 @@ const table = document.getElementById("table")
 const tbody = document.getElementById("tbody")
 
 let datos = null
+let editMode = false;
 
-fetch('http://localhost:3000/categorias')
+const urlBD = "http://localhost:3005/categorias"
+
+// Traer todas las categorias de la BD - GET
+const traerDatos = function() {
+    fetch(urlBD)
     .then(result => {
         return result.json()
     })
     .then(categories => {
-        console.log(categories);
         categories.forEach(category => {
-            console.log(category)
+           
             const option = document.createElement("option")
             option.id = category.id
             option.textContent = category.nombre;
             option.value = category.nombre
             selectForm.appendChild(option)
-        });
-    })
+            
+        });  
+        console.log(categories)   
+    })}
+traerDatos();
+
     
-
-
-let i = 1;
-
-
-// Agregar una categoria a la lista de Opciones
-btnAdd.addEventListener("click", (e) => {
-    e.preventDefault()
-    // Total de selects
-    const options = document.querySelectorAll("option")
-    let totalOptions = options.length
-    console.log(totalOptions)
-
-    const newCategory = inputCategory.value;
-
-    const categoryOption = document.createElement("option");
-    categoryOption.setAttribute("id", `category${totalOptions}`)
-    categoryOption.value = newCategory
-    categoryOption.textContent = newCategory
-    selectForm.appendChild(categoryOption)
-
-  
-    inputCategory.value = "";
-
-    const data = {
-        id: totalOptions,
-        nombre: newCategory,
-    }
-    console.log(newCategory)
-    fetch('http://localhost:3000/categorias', {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-
-
-})
-
+// Mostrar Todas las categorias en una tabla    
 showCategory.addEventListener("click", (e)=> {
     e.preventDefault()
-
+    
     mostrarCategorias()
 })
 
 
+// Guardar una categoria - Nueva Categoria / Editar Categoria
+btnAdd.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    if(editMode === false) {
+        // Total de selects
+        let idDinamico = Date.now();
+        const options = document.querySelectorAll("option")
+        let totalOptions = options.length
+        console.log(totalOptions)
+       
+        // Agregar Categoria al HTML
+        const newCategory = inputCategory.value;
+        const categoryOption = document.createElement("option");
+        categoryOption.setAttribute("id", `category${idDinamico}`)
+        categoryOption.value = newCategory
+        categoryOption.textContent = newCategory
+        selectForm.appendChild(categoryOption)
+
+    
+        inputCategory.value = "";
+
+        // Agregar Categoria en la API
+        const data = {
+            id: idDinamico,
+            nombre: newCategory,
+        }
+        console.log(newCategory)
+        fetch(urlBD, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+    } else {
+        const data = {
+            id: inputCategory.id,
+            nombre: inputCategory.value,
+        }
+        editarBD(data, data.id)
+        editMode = false;
+    }
+})
+
+// Funcion para mandar datos de una categoria a editar
+const editarBD = function (data, id) {
+    fetch(`${urlBD}/${id}` , {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json())
+        .then(data => console.log(data) )
+}
+
+// Cuando elijo una opcion puedo eliminar (DELETE) o editar (PUT)
 selectForm.addEventListener("change", (e) => {
     console.log(e.target)
     console.log(e.target.children)
@@ -95,14 +122,27 @@ selectForm.addEventListener("change", (e) => {
         selectForm.removeChild(elementRemove)  
 
         // Lo remeuvo de JSONWebServer
-        fetch('http://localhost:3000/categorias/' + elementRemoveID, {
+        fetch(`${urlBD}/${elementRemoveID}`, {
                 method: 'DELETE',
             })
             .then(response => response.json()) 
             .then(res => console.log(res))
             })
+
+
+    // Editar Categoria      
+    btnEdit.addEventListener("click", (e) => {
+        e.preventDefault();
+        editMode = true;
+        // Rellenar Input a Editar
+        inputCategory.value = elementRemove.value
+        // Agregarle su respectivo id para luego mandarlo a la BD
+        inputCategory.id = elementRemoveID
+        })
     })
 
+
+// Mostrar todas las Categorias
 const mostrarCategorias = () => {
     console.log(table.className)
     table.classList.toggle("d-none")
@@ -127,5 +167,4 @@ const mostrarCategorias = () => {
         }
     });
 }
-
 
